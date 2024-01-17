018500967e25de70286d69f3255b024d9dcc13ee
Amalroy212 <ken940005@gmail.com>
2024-01-17 11:16:14 +0530

    ui updates

--- ui/sub/index/aboutEvent/Occation.tsx
+++ ui/sub/index/aboutEvent/Occation.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
+"use client";
+
 import React from "react";
 import BgCards from "../bgCards/BgCards";
+import { motion } from "framer-motion";
+import { zoomIn } from "@/utils/viewMotions";
 
 function Occation() {
   const cardsData = [
@@ -31,13 +34,17 @@ function Occation() {
         {cardsData.map((card, index) => (
           <BgCards data={card} index={index} />
         ))}
-        <div className="relative flex flex-col md:w-1/4 w-full md:h-[50vh] h-auto justify-center items-center p-5 md:mt-[-8rem]">
-        <h1 className="absolute left-3 md:mt-[-9rem] text-9xl md:m-[-1rem] font-bold text-slate-200 z-10">
-          $1 B
-        </h1>
-        <p className="font-bold text-1xl text-slate-300 absolute w-[50%] z-10 mt-10 bg-black ml-7">
+        <motion.div 
+         variants={zoomIn(0.5,0.7)}
+         initial="hidden"
+         whileInView="show"
+         className="relative flex flex-col md:w-1/4 w-full h-[50vh] justify-center items-center p-5 md:mt-[-8rem]">
+          <h1 className="absolute left-3 md:top-0 mt-[-9rem] text-9xl md:m-[-1rem] font-bold text-slate-200 z-10">
+            $1 B
+          </h1>
+          <p className="font-bold text-1xl text-slate-300 absolute w-[50%] z-10 mt-10 ml-7">
             Bengaluru's EdTech markets estimated worth currently
-        </p>
+          </p>
           <div className="absolute top-0 left-0 w-full h-full z-0">
             <video
               loop
@@ -47,7 +54,7 @@ function Occation() {
               src="/videos/cards_bg.mp4/"
             ></video>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- ui/sub/index/bgCards/BgCards.tsx
+++ ui/sub/index/bgCards/BgCards.tsx
@@ -13,7 +13,7 @@ function BgCards({data, index} : IBgCard) {
         whileInView="visible"
         className="flex-col md:w-1/4 w-full md:h-[50vh] justify-start items-start">
       <div className="flex justify-center items-center p-0 m-0 h-full w-full relative">
-        <h1 className="absolute left-3 md:mt-[-9rem] text-7xl md:m-[-1rem] font-bold text-slate-200">
+        <h1 className="absolute left-3  md:mt-[-9rem] mt-[-9rem] text-7xl md:m-[-1rem] font-bold text-slate-200">
           {data.number}
         </h1>
         <p className="font-bold text-1xl text-slate-300 absolute">
