125f583d79a4a135fc1b00f3f276deace4ec61b7
suhail-mohd07 <suhailkudige786@gmail.com>
2024-01-22 12:59:03 +0530

    new-section

--- ui/sub/headers/SubHeadings.tsx
+++ ui/sub/headers/SubHeadings.tsx
@@ -5,10 +5,10 @@ interface SubHeadings{
     fontSize : number
 }
 
-function SubHeadings({heading} : SubHeadings) {
+function SubHeadings({heading,fontSize} : SubHeadings) {
   return (
     <div data-aos="flip-down" className="w-full h-[10vh] flex justify-center items-center z-[19]">
-      <h1 className="text-[28px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      <h1 className=" `md:text-[28px] text-[21px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         {heading}
       </h1>
     </div>
