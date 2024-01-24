46f7ce3506cab722c3f4281f1551b2cfe8d17ea7
suhail-mohd07 <suhailkudige786@gmail.com>
2024-01-24 14:13:55 +0530

    Mobile Navbar changes

--- ui/main/mobileNav/MobileNav.tsx
+++ ui/main/mobileNav/MobileNav.tsx
@@ -5,14 +5,14 @@ import { RiInstagramFill } from "react-icons/ri";
 import { FaLinkedin } from "react-icons/fa";
 import React from 'react'
 
-function MobileNav() {
+function MobileNav({ setPopup } : any) {
     return (
         <div className='w-full left-0 fixed h-screen backdrop-blur md:hidden flex justify-center items-center pb-[5rem]  px-3 z-[999]'>
             <div className='w-full h-full bg-slate-800 flex flex-col px-10 rounded-2xl py-5'>
-                <a href="#about-us" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>About us</a>
-                <a href="#happening" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Happening</a>
-                <a href="#attendies" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Attendies</a>
-                <a href="#topics" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Topics</a>
+                <a onClick={() => setPopup(false)} href="#about-us" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>About us</a>
+                <a onClick={() => setPopup(false)} href="#happening" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Happening</a>
+                <a onClick={() => setPopup(false)} href="#attendies" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Attendies</a>
+                <a onClick={() => setPopup(false)} href="#topics" className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Topics</a>
                 <Link href={"Sponosors"} className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Sponosors</Link>
                 <Link href={"Speakers"} className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Speakers</Link>
                 <Link href={"Delegates"} className='cursor-pointer text-[18px] text-center py-2  w-auto mx-15 my-2 rounded-lg text-white bg-gradient-to-r from-purple-500 to-cyan-500'>Delegates</Link>
