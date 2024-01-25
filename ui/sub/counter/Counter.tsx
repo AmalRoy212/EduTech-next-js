b2a4c5b45a56bce84b1673c8c30861987100bc14
Amalroy212 <ken940005@gmail.com>
2024-01-25 12:04:03 +0530

    ups

--- ui/sub/counter/Counter.tsx
+++ ui/sub/counter/Counter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Counter() {
   return (
-    <div data-aos='fade-up' data-aos-duration={3000} className='w-full h-auto flex absolute justify-center items-center z-20 md:top-0 md:mt-[4rem] mt-[47rem]'>
+    <div data-aos='fade-up' data-aos-duration={3000} className='w-full h-auto flex absolute justify-center items-center z-20 md:top-0 md:mt-[4rem] mt-[49rem]'>
         <div className='w-full md:h-[130px] h-[120px] rounded-2xl bg-gradient-to-r from-purple-800 to-cyan-00 flex  p-1'>
            <div className='w-[25%] h-full rounded-2xl flex flex-col justify-center items-center'>
                 <h1 className='text-white mb-5 md:text-[40px] text-[30px]'>20</h1>
