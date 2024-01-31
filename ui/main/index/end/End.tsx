efb5b453e1d6017bcdf77a6118269d9879fd0ae3
Amalroy212 <ken940005@gmail.com>
2024-01-31 18:48:55 +0530

    register button

--- ui/main/index/end/End.tsx
+++ ui/main/index/end/End.tsx
@@ -24,7 +24,7 @@ function End() {
                 </div>
 
                 <div style={{ backgroundColor: "rgba(3, 0, 20, 0.674)" }} className='w-full md:w-[70%] h-[60%] rounded-xl absolute top-[11rem] flex-col justify-center items-center md:px-[50px] z-[9]'>
-                    <p className='text-[14px] text-slate-400 px-5 md:mt-[10rem] mt-[2rem] md:p-10 text-justify'>
+                    <p className='text-[14px] text-slate-400 px-5  mt-[2rem] md:p-10 text-justify'>
                         Live interactive workshops organized by ParXcellence will be held at EduTech Oman 2040 conference. The objective is to empower educators on various advancements in the education and training sector. It is a series of professional development workshops for educators in Oman. Power-packed sessions are conducted over 2 days. The workshops are attended by teachers, trainers, senior management, academicians and professors of education institutions from all across the country.
 
                         EduTech Oman 2040 Workshops allow teachers, trainers and faculty members to explore multiple facets of:
@@ -38,7 +38,7 @@ function End() {
 
                        
                 </div>
-                <button className='w-[120px] absolute  bottom-[23rem] z-[20] h-[35px] cursor-pointer shadow-md shadow-[#7042f861]/50 text-white rounded-3xl border-[#7042f861] bg-gradient-to-r from-purple-800 to-transparent mt-4 text-[16px] hover:from-transparent hover:to-purple-800'>
+                <button className='w-[120px] absolute md:bottom-[10rem]  bottom-[23rem] z-[20] h-[35px] cursor-pointer shadow-md shadow-[#7042f861]/50 text-white rounded-3xl border-[#7042f861] bg-gradient-to-r from-purple-800 to-transparent mt-4 text-[16px] hover:from-transparent hover:to-purple-800'>
                             Register now
                         </button>
 
