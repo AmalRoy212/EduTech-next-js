87d58c48dfcc5a104fcf6093d390871948ee75d9
Amal roy <ken940005@gmail.com>
2024-03-11 16:43:58 +0530

    clean ups

--- ui/main/index/aboutUs/AboutUs.tsx
+++ ui/main/index/aboutUs/AboutUs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutUs() {
   return (
     <div data-aos="zoom-in" data-aos-duration={400} className='w-full h-[60vh]  md:h-screen md:py-10 p-5 md:px-[200px] '>
-        <p className='text-slate-400 text-justify md:p-10 md:px-[5rem] '>
+        <p className='text-slate-400 text-justify md:p-10 md:px-[5rem] mb-[4rem] '>
             Genfinity Global specializes in organizing global B2B conferences and exhibitions, collaborating with both private and government entities to catalyze transformative journeys for organizations. Our mission is to craft dynamic business platforms that foster innovation, facilitate expansion of client bases, provide opportunities to learn about cutting-edge technologies, and encourage the exchange of best practices worldwide. With a commitment to excellence, we strive to create impactful events that connect industry leaders, foster collaboration, and drive positive change on a global scale.
         </p>
     </div>
